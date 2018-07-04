851dbca80ec430ea8c5761c5e038682ab53fdc53
smarty1palak <smarty1palak@gmail.com>
2018-07-04 16:57:38 +0530

    Finished static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,38 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Twirail"
+  end
+
+  test "should get root" do
+    get FILL_IN
+    assert_response FILL_IN
+  end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Twirail"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | Twirail"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | Twirail"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | Twirail"
   end
 
 end
