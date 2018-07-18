d2729f220dbe5650ed9f29b17369afeadc1bb917
smarty1palak <smarty1palak@gmail.com>
2018-07-18 14:24:54 +0530

    user microposts

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'turbolinks',   '5.0.1'
 gem 'jbuilder',     '2.7.0'
 gem 'bcrypt',       '3.1.12'
 gem 'faker',        '1.7.3'
+gem 'carrierwave',             '1.2.2'
+gem 'mini_magick',             '4.7.0'
 gem 'will_paginate',           '3.1.6'
 gem 'bootstrap-will_paginate', '1.0.0'
 
@@ -36,6 +38,7 @@ end
 
 group :production do
   gem 'pg', '0.18.4'
+  gem 'fog', '1.42'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,7 @@ class UserTest < ActiveSupport::TestCase
 
   def setup
     @user = User.new(name: "Example User", email: "user@example.com",
-                      password: "palak", password_confirmation: "palak")
+                      password: "palak1", password_confirmation: "palak1")
   end
   test "should be vaild" do
     assert @user.valid?
@@ -69,5 +69,13 @@ class UserTest < ActiveSupport::TestCase
   end
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?(:remember, '')
+  end
+
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
   end
 end
